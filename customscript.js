3827e7e6071bf124a39e809e5c0c81508dcbc227
Felipe Marinho <felipegigamail@gmail.com>
2023-09-28 09:30:19 -0300

    remove cpf

--- customscript.js
+++ customscript.js
@@ -1,1 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  // const zap = document.querySelectorAll("[data-edit='exibirFaleConosco']")[0];
+  // const clone = zap.cloneNode(true);
+  // clone.classList.add('zaprodape');
+  // zap.style.setProperty('display', 'none');
+  const dadosrodape = document.querySelectorAll(
+    "[data-edit='corFundoRodapeBloco2']"
+  )[0].childNodes[1];
+  const cpf = dadosrodape.childNodes[5];
+  dadosrodape.removeChild(cpf);
+  // dadosrodape.appendChild(clone);
+});
